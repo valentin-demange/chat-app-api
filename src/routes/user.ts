d45952d9ad8c5b385c00d12f1581d8fbccbd3933
valentin-demange <demange.valentin@gmail.com>
2023-01-03 17:35:36 +0100

    Rename routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,16 +3,13 @@ import { Router } from 'express';
 const router = Router();
 
 router.post('/signup', (req, res) => {
-  return res.send("API signup (post) is working !");
+  return res.send("POST signup is working !");
 });
 router.post('/login', (req, res) => {
-  return res.send("API login (post) is working !");
-});
-router.get('/login', (req, res) => {
-  return res.send("API login (get) is working !");
+  return res.send("POST login is working !");
 });
 router.post('/logout', (req, res) => {
-  return res.send("API logout (post) is working !");
+  return res.send("POST logout is working !");
 });
 
 export default router;
