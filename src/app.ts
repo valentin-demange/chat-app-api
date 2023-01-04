707d98bd36714e4069cb490593aa7e88deeb2c29
valentin-demange <demange.valentin@gmail.com>
2023-01-04 17:09:58 +0100

    Change urlencoded to false

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,7 @@ import routes from './routes';
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
+app.use(express.urlencoded({ extended: false }));
 app.use('/api/users', routes.user)
 
 export default app
