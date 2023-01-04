f17aeb1fed381c76e5af19b84c257259da47f255
valentin-demange <demange.valentin@gmail.com>
2023-01-04 09:02:17 +0100

    Add Prisma files

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,21 @@
 import { Router } from 'express';
+import { Prisma, PrismaClient } from '@prisma/client'
 
 const router = Router();
+const prisma = new PrismaClient()
 
-router.post('/signup', (req, res) => {
-  return res.send("POST signup is working !");
-});
+router.post(`/signup`, async (req, res) => {
+  const { username, password } = req.body
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      published: false,
+      author: { connect: { email: authorEmail } },
+    },
+  })
+  res.json(result)
+})
 router.post('/login', (req, res) => {
   return res.send("POST login is working !");
 });
