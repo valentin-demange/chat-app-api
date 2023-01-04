a3d846a7991e13cba214e9b3857c7738db526dc5
valentin-demange <demange.valentin@gmail.com>
2023-01-04 16:34:23 +0100

    Add Prisma Database

---
+++ favorite_cmd.md
@@ -0,0 +1,7 @@
+# Heroku
+heroku pg:credentials:rotate DATABASE -a blabla-19-90
+
+# Prisma
+npx prisma db push
+npx prisma format
+
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,18 +4,6 @@ import { Prisma, PrismaClient } from '@prisma/client'
 const router = Router();
 const prisma = new PrismaClient()
 
-router.post(`/signup`, async (req, res) => {
-  const { username, password } = req.body
-  const result = await prisma.post.create({
-    data: {
-      title,
-      content,
-      published: false,
-      author: { connect: { email: authorEmail } },
-    },
-  })
-  res.json(result)
-})
 router.post('/login', (req, res) => {
   return res.send("POST login is working !");
 });
