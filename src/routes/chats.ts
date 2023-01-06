99e4c075673333315abe39da79aa1aabd1c03aa5
valentin-demange <demange.valentin@gmail.com>
2023-01-06 18:50:52 +0100

    Add APIs to get chat info & user info

--- src/routes/chats.ts
+++ src/routes/chats.ts
@@ -17,5 +17,21 @@ router.get("/:chatId/messages", async (req, res) => {
     res.status(500).send({ error: "An error occurred while retrieving messages" });
   }
 });
+router.get('/:chatId', async (req, res) => {
+  const { chatId } = req.params;
+  try {
+    const chat = await prisma.chat.findUnique({
+      where: { id: Number(chatId) },
+      include: { members: false, messages: false },
+    });
+    if (!chat) {
+      return res.status(404).send({ error: 'Chat not found' });
+    }
+    return res.send(chat);
+  } catch (error) {
+    return res.status(500).send({ error: 'An error occurred' });
+  }
+});
+
 
 export default router;
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { Prisma, PrismaClient } from "@prisma/client";
 const bcrypt = require("bcryptjs");
 const passport = require("passport");
-
+const _ = require('lodash');
 const router = Router();
 const prisma = new PrismaClient();
 
@@ -83,6 +83,21 @@ router.get('/:userId/chats', async (req, res) => {
     res.send(chatIds);
   } catch (error) {
     res.status(500).send(error);
+  }
+});
+router.get('/:userId', async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: Number(userId) },
+    });
+    if (!user) {
+      return res.status(404).send({ error: 'User not found' });
+    }
+    const userWithoutPassword = _.omit(user, ['password']);
+    return res.send(userWithoutPassword);
+  } catch (error) {
+    return res.status(500).send({ error: 'An error occurred' });
   }
 });
 
