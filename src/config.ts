e7f7c45387c467bba1b9958a00979baf24ff7d28
valentin-demange <demange.valentin@gmail.com>
2023-01-14 18:33:04 +0100

    Add the membership to general chatroom on signup

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,4 @@ export const PORT = process.env.PORT || 3000
 export const NODE_ENV = process.env.NODE_ENV || 'dev'
 export const SESSION_SECRET = process.env.SESSION_SECRET || 'anything'
 export const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3001'
+export const GENERAL_CHAT_ID = process.env.GENERAL_CHAT_ID || "1"
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { Prisma, PrismaClient } from "@prisma/client";
+import { GENERAL_CHAT_ID } from "../config";
 const bcrypt = require("bcryptjs");
 const passport = require("passport");
-const _ = require('lodash');
+const _ = require("lodash");
 const router = Router();
 const prisma = new PrismaClient();
 
-router.get('/test', function(req:any, res, next) {
-  console.log(req.user)
-  return res.status(200).send("OK")
+router.get("/test", function (req: any, res, next) {
+  console.log(req.user);
+  return res.status(200).send("OK");
 });
 
 router.post("/sign-up", async (req, res, next) => {
@@ -19,8 +20,9 @@ router.post("/sign-up", async (req, res, next) => {
       password,
       10,
       async (err: any, hashedPassword: string) => {
+        let result;
         try {
-          const result = await prisma.user.create({
+          result = await prisma.user.create({
             data: {
               email,
               password: hashedPassword,
@@ -28,16 +30,34 @@ router.post("/sign-up", async (req, res, next) => {
               lastName,
             },
           });
-          res.status(200).send("Sign-up successful");
-        } catch (error:any) {
+        } catch (error: any) {
           if (error.code === "P2002") {
             // user with the given email already exists
             res.status(409).send("A user with the given email already exists");
           } else {
-            res.status(500).send("There was an error while creating a new user");
+            res
+              .status(500)
+              .send("There was an error while creating a new user");
           }
           return next(error);
         }
+
+        try {
+          const newMember = await prisma.member.create({
+            data: {
+              user: {
+                connect: { id: result.id },
+              },
+              chat: {
+                connect: { id: Number(GENERAL_CHAT_ID) },
+              },
+            },
+          });
+          res.status(200).send("User has been created");
+        } catch (err:any) {
+          res.status(500).send("User has been successfully created but it has not been added to the general chat room");
+        }
+
       }
     );
   } catch (error) {
@@ -66,20 +86,19 @@ router.post("/login", (req, res, next) => {
 });
 router.get("/logout", (req, res, next) => {
   // @ts-ignore
-  req.logout(function(err){
+  req.logout(function (err) {
     if (err) {
       console.log(err);
       return next(err);
     }
-    return res.status(200).send({ message: 'Successfully logged out' })
+    return res.status(200).send({ message: "Successfully logged out" });
   });
-  
 });
 
-router.get('/current/chats', async (req:any, res) => {
+router.get("/current/chats", async (req: any, res) => {
   try {
     if (!req.user) {
-      return res.status(404).send('User not found');
+      return res.status(404).send("User not found");
     }
     const userId = req.user.id;
     // const userId = 1;
@@ -92,7 +111,7 @@ router.get('/current/chats', async (req:any, res) => {
     res.status(500).send(error);
   }
 });
-router.get('/current', async (req:any, res) => {
+router.get("/current", async (req: any, res) => {
   // const { userId } = req.params;
   try {
     // const user = await prisma.user.findUnique({
@@ -101,16 +120,16 @@ router.get('/current', async (req:any, res) => {
 
     const user = req.user;
     if (!user) {
-      return res.status(404).send('User not found');
+      return res.status(404).send("User not found");
     }
-    const userWithoutPassword = _.omit(user, ['password']);
+    const userWithoutPassword = _.omit(user, ["password"]);
     return res.send(userWithoutPassword);
   } catch (error) {
-    return res.status(500).send('An error occurred');
+    return res.status(500).send("An error occurred");
   }
 });
 
-router.get('/:userId', async (req:any, res) => {
+router.get("/:userId", async (req: any, res) => {
   const { userId } = req.params;
   try {
     const user = await prisma.user.findUnique({
@@ -119,16 +138,13 @@ router.get('/:userId', async (req:any, res) => {
 
     // const user = req.user;
     if (!user) {
-      return res.status(404).send('User not found');
+      return res.status(404).send("User not found");
     }
-    const userWithoutPassword = _.omit(user, ['password']);
+    const userWithoutPassword = _.omit(user, ["password"]);
     return res.send(userWithoutPassword);
   } catch (error) {
-    return res.status(500).send('An error occurred');
+    return res.status(500).send("An error occurred");
   }
 });
-
-
-
 
 export default router;
