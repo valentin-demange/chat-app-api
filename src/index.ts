6f91a0fd2aa5a625ae686dde0d0529687761d3d1
valentin-demange <demange.valentin@gmail.com>
2023-01-14 13:38:31 +0100

    Add socket to support chat creation

--- src/index.ts
+++ src/index.ts
@@ -1,84 +1,47 @@
-import { Message } from '@prisma/client';
-import http from 'http'
-import app from './app'
-import { PORT } from './config';
-require('dotenv').config()
-const socketio = require('socket.io');
+import { Message } from "@prisma/client";
+import http from "http";
+import app from "./app";
+import { PORT } from "./config";
+require("dotenv").config();
+const socketio = require("socket.io");
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
-
+  console.log(`Server running on port ${PORT}`);
+});
 
 const io = socketio(server);
 
-io.on('connection', (socket:any) => {
-  console.log("New socket connection")
-  socket.on('send message', (message:Message, chatId:number) => {
-    console.log(`New message in room n°${chatId}`)
-    io.to(chatId).emit('new message', message);
-  });
-  socket.on('join chat room', (chatId:number) => {
-    console.log(`New user in room n°${chatId}`)
+io.on("connection", (socket: any) => {
+  console.log("New socket connection");
+
+  // Socket to display new messages when they are sent
+  socket.on("join chat room", (chatId: number) => {
+    console.log(`New user in room n°${chatId}`);
     socket.join(chatId);
   });
-  socket.on('leave chat room', (chatId:number) => {
-    console.log(`User leaving room n°${chatId}`)
+  socket.on("leave chat room", (chatId: number) => {
+    console.log(`User leaving room n°${chatId}`);
     socket.leave(chatId);
+  });
+  socket.on("send message", (message: Message, chatId: number) => {
+    console.log(`New message in room n°${chatId}`);
+    io.to(chatId).emit("new message", message);
+  });
+
+  // Socket to display new chats when they are created
+  socket.on("create user socket", (userId: number) => {
+    console.log(`Create socket for user n°${userId}`);
+    socket.join(userId);
+  });
+  socket.on("remove user socket", (userId: number) => {
+    console.log(`Remove socket for user n°${userId}`);
+    socket.leave(userId);
+  });
+  socket.on("new chat", (userId: number, chatId: number) => {
+    console.log(`New chat for user n°${userId}`);
+    io.to(userId).emit("new chat", chatId);
   });
 });
 
-// // When a new socket connection is established
-// io.on('connection', (socket:any) => {
-//   // Print a message to the console
-//   console.log('New socket connection');
-
-//   // // When the client sends a message
-//   // socket.on('message', (message:string) => {
-//   //   // Print the message to the console
-//   //   console.log(`Received message: ${message}`);
-
-//   //   // Send the message to all sockets
-//   //   io.emit('message', message);
-//   // });
-
-//   socket.on('disconnect', () => {
-//     console.log('🔥: A user disconnected');
-//   });
-// });
-
-// io.on('connection', (socket: any) => {
-//   socket.on('subscribe chats', (userId:string) => {
-//     socket.join(userId)
-//   })
-
-//   socket.on('create chat', (userId:string) => {
-//     socket.to(userId).emit('chat created')
-//   })
-
-//   socket.on('delete chat', (userId:string) => {
-//     socket.to(userId).emit('chat deleted')
-//   })
-
-//   socket.on('add member', (userId:string) => {
-//     socket.to(userId).emit('member added')
-//   })
-
-//   socket.on('leave group', (userId:string) => {
-//     socket.to(userId).emit('member left')
-//   })
-
-//   socket.on('subscribe chat messages', (chatId:string) => {
-//     socket.join(chatId)
-//   })
-
-//   socket.on('unsubscribe chat messages', (chatId:string) => {
-//     socket.leave(chatId)
-//   })
-
-//   socket.on('send message', (chatId:string, message:string) => {
-//     socket.to(chatId).emit('receive message', message)
-//   })
-// })
